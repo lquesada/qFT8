f3c46c5a61c09f94e6c76c49c7d08f89d3e23ef9
Luis Quesada <elezeta@gmail.com>
2026-09-03 23:27:07 +0200

    Update

--- docs/data.js
+++ docs/data.js
@@ -1,7 +1,7 @@
 // qFT8 Global Release and App Metadata Configuration
-const LATEST_VERSION = "2.51";
-const LATEST_DATE = "2026-09-02";
-const LATEST_SIZE = "7.30MB";
+const LATEST_VERSION = "2.52";
+const LATEST_DATE = "2026-09-03";
+const LATEST_SIZE = "7.33MB";
 
 // Supported application languages
 const supportedLanguages = ['en', 'es', 'pt', 'fr', 'it', 'de', 'ru', 'zh', 'ja', 'eo', 'ro', 'hi', 'ar', 'ca', 'eu', 'gl', 'id', 'ko', 'bn', 'ur', 'vi', 'tr', 'nl', 'pl', 'ha', 'cs', 'uk', 'sv', 'fi', 'da', 'he', 'hu', 'no', 'sk', 'th'];
